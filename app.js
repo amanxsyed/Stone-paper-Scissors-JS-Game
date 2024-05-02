209092ad7fdc10b027d35696453f550abda4f2c5
Syed <amanxsyed@gmail.com>
2024-05-02 16:33:36 +0500

    made the function of js to generate random numbers limit to 2

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let userScore = 0;
 let aiBotScore = 0;
 const choices = document.querySelectorAll('.choice');
+const genCompChoice = () => {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIdx = Math.floor(Math.random() * 3);
+    return choices[randomIdx];
+}
 const playGame= (userChoice) => {
     console.log("User Choice = ", userChoice);
 
